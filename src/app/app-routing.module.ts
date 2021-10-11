4bf5ca0b2a8a050e26a0c2ac36711985f4550464
pujie <pw.prayitno@gmail.com>
2021-10-11 16:36:10 +0700

    add html formatting,proxies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LazyComponent } from './lazy/lazy.component';
 import { SearchticketComponent } from './searchticket/searchticket.component';
 const routes: Routes = [
-  {
+  {path:'search',
+  component:SearchticketComponent
+  },
+{
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
+    {
     path: 'message/:id',
     loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'search',
     pathMatch: 'full'
   },
   {path:'lazy',
   component:LazyComponent
-  },
-  {path:'search',
-  component:SearchticketComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HttpHandler } from '@angular/common/http';
 import { LazyComponent } from './lazy/lazy.component';
 import { SearchticketComponent } from './searchticket/searchticket.component';
-
+import { EditorModule } from '@tinymce/tinymce-angular'
 @NgModule({
   declarations: [AppComponent,LazyComponent,SearchticketComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule, EditorModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },HttpClientModule],
   bootstrap: [AppComponent],
 })
